214637c24bbd86a7e10bf8f54a459435b543d7c9
Lukas Loesche <lukas@mesosphere.io>
2018-09-25 14:03:39 +0200

    Automatic update of variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "bootstrap_port" {
   default     = "80"
-  description = "bootstrap servers port"
+  description = "TCP port bootstrap nginx is listening on. Used to build the bootstrap URL."
 }
 
 variable "bootstrap_private_ip" {
-  description = "bootrstrap server private ip"
+  description = "Private IP bootstrap nginx is listening on. Used to build the bootstrap URL."
 }
 
 variable "dcos_install_mode" {
   default     = "install"
-  description = "specifies which type of command to execute. Options: `install` or `upgrade`"
+  description = "Type of command to execute. Options: install or upgrade"
 }
 
 variable "dcos_skip_checks" {
@@ -19,11 +19,11 @@ variable "dcos_skip_checks" {
 
 variable "dcos_version" {
   default     = "1.11.3"
-  description = "specifies which dcos version instruction to use. Options: `1.9.0`, `1.8.8`, etc. _See [dcos_download_path](https://github.com/dcos/tf_dcos_core/blob/master/download-variables.tf) or [dcos_version](https://github.com/dcos/tf_dcos_core/tree/master/dcos-versions) tree for a full list._"
+  description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
 }
 
 variable "num_masters" {
-  description = "Number of masters"
+  description = "Specify the amount of masters. For redundancy you should have at least 3"
 }
 
 variable "master_ips" {
@@ -33,15 +33,15 @@ variable "master_ips" {
 
 variable "os_user" {
   default     = "centos"
-  description = "The OS user to be used with ssh exec"
+  description = "The OS user to be used"
 }
 
 variable "trigger" {
   default     = []
-  description = "triggers for null resource"
+  description = "Triggers for null resource"
 }
 
 variable "depends_on" {
   default     = []
-  description = "modules are missing the depends_on feature. Faking this feature with input and output variables"
+  description = "Modules are missing the depends_on feature. Faking this feature with input and output variables"
 }
